10cad0a26e4b7288a9277dc0a46e872ef001674d
STUTI-01 <59913235+STUTI-01@users.noreply.github.com>
2021-07-14 20:53:46 +0530

    just audio player clipping done

--- lib/test.dart
+++ lib/test.dart
@@ -13,22 +13,144 @@ class Test extends StatefulWidget {
 
 class _TestState extends State<Test> {
   late AudioPlayer player;
+  @override
+  void initState() {
+    super.initState();
+    initialisePlayer();
+  }
+
   void initialisePlayer() async {
     player = AudioPlayer();
     await player.setUrl(
         'https://firebasestorage.googleapis.com/v0/b/dhvani-aa814.appspot.com/o/NAVAGRAHA_MANTRAS.mp3?alt=media&token=0e7913fc-c82c-4c39-b3a3-3c1064e2f0a4');
-    player.play();
+  }
+
+  void trimSong() async {
+    await player.setClip(
+        start: Duration(seconds: widget.start.round()),
+        end: Duration(seconds: widget.end.round()));
+    await player.play();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: GestureDetector(
-        child: Text("PLAY"),
-        onTap: () {
-          initialisePlayer();
-        },
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.only(
+          top: 0.2 * height,
+          left: 0.2 * width,
+        ),
+        child: Column(
+          children: [
+            Container(
+                color: Colors.blue.withOpacity(0.6),
+                height: 200,
+                width: 300,
+                child: ControlButtons(player, trimSong)),
+            GestureDetector(
+              child: const Text("PLAY"),
+              onTap: () {
+                initialisePlayer();
+              },
+            ),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+/// Displays the play/pause button and volume/speed sliders.
+class ControlButtons extends StatelessWidget {
+  final AudioPlayer player;
+  final Function trim;
+
+  ControlButtons(this.player, this.trim);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        // Opens volume slider dialog
+        IconButton(
+          icon: Icon(Icons.volume_up),
+          onPressed: () {
+            // showSliderDialog(
+            //   context: context,
+            //   title: "Adjust volume",
+            //   divisions: 10,
+            //   min: 0.0,
+            //   max: 1.0,
+            //   stream: player.volumeStream,
+            //   onChanged: player.setVolume,
+            // );
+          },
+        ),
+
+        /// This StreamBuilder rebuilds whenever the player state changes, which
+        /// includes the playing/paused state and also the
+        /// loading/buffering/ready state. Depending on the state we show the
+        /// appropriate button or loading indicator.
+        StreamBuilder<PlayerState>(
+          stream: player.playerStateStream,
+          builder: (context, snapshot) {
+            final playerState = snapshot.data;
+            final processingState = playerState?.processingState;
+            final playing = playerState?.playing;
+            if (processingState == ProcessingState.loading ||
+                processingState == ProcessingState.buffering) {
+              return Container(
+                margin: EdgeInsets.all(8.0),
+                width: 64.0,
+                height: 64.0,
+                child: CircularProgressIndicator(),
+              );
+            } else if (playing != true) {
+              return IconButton(
+                icon: Icon(Icons.play_arrow),
+                iconSize: 64.0,
+                onPressed: () {
+                  trim();
+                },
+              );
+            } else if (processingState != ProcessingState.completed) {
+              return IconButton(
+                icon: Icon(Icons.pause),
+                iconSize: 64.0,
+                onPressed: player.pause,
+              );
+            } else {
+              return IconButton(
+                icon: Icon(Icons.replay),
+                iconSize: 64.0,
+                onPressed: () => player.seek(Duration.zero),
+              );
+            }
+          },
+        ),
+        // Opens speed slider dialog
+        StreamBuilder<double>(
+          stream: player.speedStream,
+          builder: (context, snapshot) => IconButton(
+            icon: Text("${snapshot.data?.toStringAsFixed(1)}x",
+                style: const TextStyle(fontWeight: FontWeight.bold)),
+            onPressed: () {
+              // showSliderDialog(
+              //   context: context,
+              //   title: "Adjust speed",
+              //   divisions: 10,
+              //   min: 0.5,
+              //   max: 1.5,
+              //   stream: player.speedStream,
+              //   onChanged: player.setSpeed,
+              // );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
